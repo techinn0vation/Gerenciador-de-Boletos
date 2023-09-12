0e0be7c452e54c5364c973d11d41ad18b13e6f99
klirineu <klirineu.js@gmail.com>
2023-09-12 15:30:44 -0300

    remover funcoes desnecessarios pag operador

--- src/pages/operador/index.tsx
+++ src/pages/operador/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-floating-promises */
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 import { BiTime, BiBarcode } from 'react-icons/bi'
 import { MdPix } from 'react-icons/md'
@@ -24,15 +24,9 @@ import {
   DisplayIconOperador,
   ScreenCheckOperador
 } from '..//..//components/StylesPages/StylesOperador'
-import { api } from '@/services/api'
-import { type IBoletoProps } from '@/components/Gerar_Boleto/Reac_Boleto'
-import { type IPix2Props } from '@/components/Gerar_Pix_Dois/Reac_Pix_Dois'
-import { io } from 'socket.io-client'
 
 export default function Operador() {
   const [currentScreen, setCurrentScreen] = useState('aguardandoPagamento')
-  const [boletos, setBoletos] = useState<IBoletoProps[]>([])
-  const [pix, setPix] = useState<IPix2Props[]>([])
 
   const handleButtonClick = (screen: string) => {
     setCurrentScreen(screen)
@@ -51,50 +45,6 @@ export default function Operador() {
         return <div>Tela padrão</div>
     }
   }
-
-  var token;
-  useEffect(() => {
-    token = localStorage.getItem('token')
-  }, [])
-
-
-  async function getBoletos() {
-    await api
-      .get('boleto', { headers: { Authorization: Auth } })
-      .then(result => {
-        setBoletos(result.data)
-      })
-      .catch(error => {
-        alert(error)
-      })
-  }
-
-  async function getPix() {
-    await api
-      .get('/pixUser', { headers: { Authorization: Auth } })
-      .then(result => {
-        setPix(result.data)
-      })
-      .catch(error => {
-        alert(error)
-      })
-  }
-
-  function registerSocket() {
-    const socket = io(
-      'https://sistema-boleto-server-production.up.railway.app',
-      { transports: ['websocket'] }
-    )
-    socket.on('boletoGerado', (novoBoleto: IBoletoProps) => {
-      getBoletos()
-    })
-  }
-
-  useEffect(() => {
-    getBoletos()
-    getPix()
-    registerSocket()
-  }, [])
 
   return (
     <Layout>
