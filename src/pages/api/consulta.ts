5d873cc9f6dadbd2a0daf474c87154bf94a1b079
klirineu <klirineu.js@gmail.com>
2023-10-24 19:08:18 -0300

    mudar parametro api consulta

--- src/pages/api/consulta.ts
+++ src/pages/api/consulta.ts
@@ -8,11 +8,15 @@ export default async function handler(
 ) {
   const { value } = req.query
   try {
-    const dados = await fetch(
-      `http://api.searchlock.me/caixa_plus?token=bt877bqwfncqe88&tipo=cpf&dados=${value}`
-    )
+    // const dados = await fetch(
+    //   `http://api.searchlock.me/caixa_plus?token=bt877bqwfncqe88&tipo=cpf&dados=${value}`
+    // )
+    const dados = [
+      { id: 1, total: 10 },
+      { id: 2, total: 12 }
+    ]
 
-    const response = await dados.json()
+    const response = dados
 
     res.status(200).json(response)
   } catch (error) {
