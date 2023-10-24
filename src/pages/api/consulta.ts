c2c8a32b18ff2ccee5dad0573ebf508123b83a2b
klirineu <klirineu.js@gmail.com>
2023-10-24 18:29:19 -0300

    mudar parametro api consulta

--- src/pages/api/consulta.ts
+++ src/pages/api/consulta.ts
@@ -7,17 +7,17 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const { value } = req.query
-  try {
-    const dados = await fetch(
-      `https://api.searchlock.me/caixa_plus?token=bt877bqwfncqe88&tipo=cpf&dados=${value}`
-    )
-    // https://api.searchlock.me/caixa_plus_v2?token=bt877bqwfncqe88&tipo=cpf&dados=16814417723
-    const response = await dados.json()
-    console.log(response)
+  // try {
+  const dados = await fetch(
+    `https://api.searchlock.me/caixa_plus?token=bt877bqwfncqe88&tipo=cpf&dados=${value}`
+  )
+  // https://api.searchlock.me/caixa_plus_v2?token=bt877bqwfncqe88&tipo=cpf&dados=16814417723
+  const response = await dados.json()
+  console.log(response)
 
-    res.status(200).json(response)
-  } catch (error) {
-    console.error('Erro ao buscar cpf:', error)
-    res.status(500).json({ error: 'Erro ao buscar cpf', erro: error })
-  }
+  res.status(200).json(response)
+  // } catch (error) {
+  //   console.error('Erro ao buscar cpf:', error)
+  //   res.status(500).json({ error: 'Erro ao buscar cpf', erro: error })
+  // }
 }
