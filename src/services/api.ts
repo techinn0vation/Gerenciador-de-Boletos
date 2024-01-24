2e50a8be844278ce867a574aebfd1ed01031a6c8
klirineu <klirineu.js@gmail.com>
2024-01-24 10:34:36 -0300

    altear ulr

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 // https://sistema-boleto-server-production.up.railway.app
 // http://192.168.0.6:3333
 export const api = axios.create({
-  baseURL: 'http://192.168.3.32:3333'
+  baseURL: 'https://sistema-boleto-server-production.up.railway.app'
 })
 // j07068145@gmail.com
 // J07068145@123
