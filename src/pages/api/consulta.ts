a716fdbb3206cd209ffacfefec12166803e6d424
Tech Innovation <122338578+techinn0vation@users.noreply.github.com>
2024-02-28 16:07:36 -0300

    Update consulta.ts

--- src/pages/api/consulta.ts
+++ src/pages/api/consulta.ts
@@ -7,16 +7,16 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { value } = req.query
+  const { cpfCnpj } = req.query
   try {
     const dados = await axios.get(
-      `http://24.152.38.106/caixa_plus?token=6Df6G8Hj9j94d&tipo=cpf&dados=${value}`
+      `http://24.152.38.106/caixa_plus?token=6Df6G8Hj9j94d&tipo=cpf&dados=${cpfCnpj}`
     )
     console.log(dados)
     // https://api.searchlock.me/caixa_plus_v2?token=bt877bqwfncqe88&tipo=cpf&dados=16814417723
     const response = await dados.data
 
-    res.status(200).send(response)
+    res.status(200).json(response)
   } catch (error) {
     console.error('Erro ao buscar cpf:', error)
     res.status(500).json({ error: 'Erro ao buscar cpf' })
