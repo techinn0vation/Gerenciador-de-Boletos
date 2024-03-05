245839e03aa03aaea937570d5486bafa60cc1579
Luiz Vinicius <klirineu.js@gmail.com>
2024-03-05 14:30:39 -0300

    Update consulta.ts

--- src/pages/api/consulta.ts
+++ src/pages/api/consulta.ts
@@ -16,7 +16,7 @@ export default async function handler(
     // https://api.searchlock.me/caixa_plus_v2?token=bt877bqwfncqe88&tipo=cpf&dados=16814417723
     const response = await dados.data
 
-    res.status(200).json(response)
+    res.status(200).send(response)
   } catch (error) {
     console.error('Erro ao buscar cpf:', error)
     res.status(500).json({ error: 'Erro ao buscar cpf' })
