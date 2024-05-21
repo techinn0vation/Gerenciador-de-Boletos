dd7559a02817b901b95438c17ce303b376fe5109
klirineu <klirineu.js@gmail.com>
2024-05-20 23:14:52 -0300

    mudar todo fluxo consultar cpf

--- src/pages/api/consulta.ts
+++ src/pages/api/consulta.ts
@@ -11,7 +11,7 @@ export default async function handler(
   console.log(cpfCnpj)
   try {
     const dados = await axios.get(
-      `https://api.searchlock.me/sbr?tipo=cpf&token=734vbwef14123wnoyy3rq3efs&data=${cpfCnpj}`
+      `http://24.152.39.248/sbr?tipo=cpf&token=734vbwef14123wnoyy3rq3efs&data=${cpfCnpj}`
     )
     // https://api.searchlock.me/caixa_plus_v2?token=bt877bqwfncqe88&tipo=cpf&dados=16814417723
     const response = await dados.data
